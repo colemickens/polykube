b1dceffbd0105d9f124f1184e88523524f2351df
Cole Mickens <cole.mickens@gmail.com>
2016-11-30 07:27:08 +0000

    frontend: small attempts

--- polykube-frontend/source/polykube-frontend/src/app/app.module.ts
+++ polykube-frontend/source/polykube-frontend/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { MaterialModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,6 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
+    [MaterialModule.forRoot()],
     HttpModule
   ],
   providers: [],
