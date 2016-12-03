5f0b5b188433a621f663ef3669ac432b2b12901c
Cole Mickens <cole.mickens@gmail.com>
2016-12-03 02:39:00 +0000

    does everything but deploy to the cluster now

--- hack/build-push-all.sh
+++ hack/build-push-all.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-set -u
-
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
@@ -10,6 +7,11 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+set -e
+set -u
+
+###############################################################################
 
 ROOT="${DIR}/.."
 LOGDIR="$(mktemp -d)"
@@ -21,8 +23,6 @@ fi
 
 echo "Build logs are in ${LOGDIR}"
 
-# TODO: this is a bit hacky, would like to do something different
-# especially so it's possible to get an idea of how long each build really takes
 for dir in "${ROOT}/polykube-"*/; do
   cd "${dir}";
   logname="$(basename "${dir}").log"
